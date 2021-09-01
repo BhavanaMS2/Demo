32823d6369f8f0f6e887a45582610d2831473c90
BhavanaMS2 <89844238+BhavanaMS2@users.noreply.github.com>
2021-09-01 11:36:24 +0530

    Update ViewTenant.js

--- src/components/ViewTenant.js
+++ src/components/ViewTenant.js
@@ -1,12 +1,54 @@
-import React from 'react'
+// import 'bootstrap/dist/js/popper.min.js';
+import React,{useState,useEffect} from 'react'
+import Table_View from './Table';
+
+
 
 function ViewTenant() {
+    
+    const [client, setClient] = useState(0);
+    const [user,setUser] = useState(0);
+
+    
+
+    const handleUserData=(e)=>{
+        const name=e.target.name;
+        const value=e.target.value;
+        setUser(value);
+    }
+
+
+
+    const handleClientData=(e)=>{
+        const name=e.target.name;
+        const value=e.target.value;
+        setClient(value);
+        
+    }
+
     return (
         <div>
-            View TABLE
+            <div>
+            <br/>
+            <label htmlFor="user"><b>USERS</b></label>&nbsp;&nbsp;
+            <input type="text" placeholder="Enter Number Of Users" name="user" id="user" onChange={handleUserData} required></input>&nbsp;&nbsp;&nbsp;&nbsp;
+            
+            <br/><br/>
+            <label htmlFor="client"><b>CLIENTS</b></label>&nbsp;&nbsp;
+            <input type="text" placeholder="Enter Number Of Clients" name="client" id="client" onChange={handleClientData} required></input>&nbsp;&nbsp;&nbsp;&nbsp;
+            <br/><br/>
+            <h5>"You have added {user} users and {client} clients"</h5>
+            
+            </div> 
+            
+            <br/><br/><br/> 
+          
+           <Table_View/>
+            
         </div>
     )
 }
 
 export default ViewTenant
+
 
